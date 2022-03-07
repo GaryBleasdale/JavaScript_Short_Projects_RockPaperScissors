cfd52f342fe76a7e7f9d5f5a675e9ec1f2c28840
Gary Malcolm Bleasdale <g.m.bleasdale@gmail.com>
2022-03-07 15:01:36 -0300

    added ui

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function computerPlay(){
 
 function playRound(playerSelection,computerSelection){
   playerSelection = playerSelection.toLowerCase()
-
   if (computerSelection==="Rock")
   switch (playerSelection){
    case 'rock': return 'Try again'
@@ -34,7 +33,7 @@ function game(){
  let pcScore=[];
 
  for (i=0;i<5;i++){
-   let result = playRound(window.prompt("Please select Rock, Paper or Scissors"), computerPlay())
+   let result = playRound("Rock", computerPlay())
    if (result ==="You won!"){
      myScore.push('1')
    } else if (result === "You lost!"){
@@ -49,4 +48,145 @@ function game(){
 
 }
 
-console.log(game())
+function rockCheck(){  
+  computerPlay()
+  if (computerPlay()==="Scissors"){
+    narration.innerHTML=`
+    You choose Rock, PC chose Scissors
+    You won!
+    `
+    myScore +=1;
+    scoreBoard[0].innerHTML=`
+    ${myScore}
+    `
+  } else if (computerPlay()==="Paper"){
+    narration.innerHTML=`
+    You choose Rock, PC chose Paper
+    You lost!
+    `
+    pcScore +=1;
+    scoreBoard[1].innerHTML=`
+    ${pcScore}
+    `
+  }else {
+    narration.innerHTML=`
+    You tied!  Please try again.
+    `
+    }
+    check()
+  }
+
+  function paperCheck(){
+  if (computerPlay()==="Scissors"){
+    narration.innerHTML=`
+    You choose Paper, PC chose Scissors
+    You lost!
+    `
+    pcScore +=1;
+    scoreBoard[1].innerHTML=`
+    ${pcScore}
+    `
+  } else if (computerPlay()==="Rock"){
+    narration.innerHTML=`
+    You choose Paper, PC chose Rock
+    You won!
+    `
+    myScore +=1;
+    scoreBoard[0].innerHTML=`
+    ${myScore}
+    `
+  }else {
+    narration.innerHTML=`
+    You tied!  Please try again.
+    `
+    }
+    check()
+  }
+
+  function scissorsCheck(){
+    if (computerPlay()==="Paper"){
+      narration.innerHTML=`
+      You choose Scissors, PC chose Paper
+      You won!
+      `
+      myScore +=1;
+      scoreBoard[0].innerHTML=`
+      ${myScore}
+      `
+    } else if (computerPlay()==="Rock"){
+      narration.innerHTML=`
+      You choose Scissors, PC chose Rock
+      You lost!
+      `
+      pcScore +=1;
+      scoreBoard[1].innerHTML=`
+      ${pcScore}
+      `
+    }else {
+      narration.innerHTML=`
+      You tied! Please try again.
+      `
+        
+      }
+    check()
+    }
+  
+
+
+function check(){
+  if (myScore===5){
+    buttons[0].removeEventListener("click", rockCheck);
+    buttons[1].removeEventListener("click", paperCheck);
+    buttons[2].removeEventListener("click", scissorsCheck);
+
+    finalResult.innerHTML=`
+    Congatulations!  You won the game!
+    `
+    restartBtn.style.display = "inline"
+
+    container.style.display='none'
+  }else if (pcScore ===5){
+    buttons[0].removeEventListener("click", rockCheck);
+    buttons[1].removeEventListener("click", paperCheck);
+    buttons[2].removeEventListener("click", scissorsCheck);
+
+    finalResult.innerHTML=`
+    Oh no!  The PC beat you!
+    `
+    restartBtn.style.display = "inline"
+    container.style.display='none'
+
+  }
+
+  }
+
+
+// DOM objects 
+
+let buttons = document.body.getElementsByTagName('button')
+let scoreBoard = document.body.getElementsByTagName('span');
+let narration = document.body.querySelector('.narration')
+let finalResult =document.body.querySelector('.final-result')
+let restartBtn = document.body.querySelector('.restart')
+let container = document.body.querySelector('.container')
+
+
+//Key variables
+
+let myScore = 0;
+let pcScore = 0;
+
+//Event Listeners
+
+buttons[0].addEventListener('click', rockCheck);
+
+buttons[1].addEventListener('click', paperCheck)
+
+buttons[2].addEventListener('click', scissorsCheck)
+
+restartBtn.addEventListener('click', function(){
+  
+  location.reload();
+} )
+
+
